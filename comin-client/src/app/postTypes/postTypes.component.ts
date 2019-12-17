f7cd14dda4d210ec888e5b8f78653a95e684d1d7
Tutku BAYRI <tutkubayri@gmail.com>
2019-12-17 11:30:16 +0200

    Search Results
    If result of a filter does not match other filter, it should be removed from the result list.

--- comin-client/src/app/postTypes/postTypes.component.ts
+++ comin-client/src/app/postTypes/postTypes.component.ts
@@ -8,6 +8,7 @@ import { FormArea } from '../formArea';
 import { Observable } from 'rxjs';
 import { Post } from '../post';
 import { FormAreaService } from '../services/form-area.service';
+import { WikiData } from '../wikiData';
 
 @Component({
   selector: 'app-postTypes',
@@ -20,16 +21,29 @@ export class PostTypesComponent implements OnInit {
   postType: PostType;
   formAreas: String [];
   formAreaInstanceAddForm: FormGroup;
+  tagForm: FormGroup;
   post: Post;
   submitted = false;
   jsonVersion: JSON;
+  tagArray: Object;
+  suggestionTags: WikiData[];
+  tag:string;
+  arr: string[];
+  tags: WikiData[];
+  importTag: WikiData;
+  list = false;
 
   constructor(private route: ActivatedRoute, private postTypeService: PostTypeService,
     private postService: PostService, private formBuilder: FormBuilder, private formAreaService: FormAreaService) { }
 
   createFormAreaInstanceAddForm(postType: PostType) {
     this.postType = postType;
+    this.tagForm = this.formBuilder.group({
+      semanticTag:["",Validators.required],
+      selectedTags:["",Validators.required],
+    })
     this.formAreaInstanceAddForm = this.formBuilder.group({
+      
     });
     for (let j = 0; j < postType.formAreas.length; j++) {
       this.formAreaInstanceAddForm.addControl(postType.formAreas[j].label, new FormControl('', Validators.required));
@@ -51,9 +65,30 @@ export class PostTypesComponent implements OnInit {
       }, error => console.log(error));
   }
 
+  tagSearch(){
+    this.arr = this.tagForm.get("semanticTag").value.split(" ");
+    this.arr.push(this.tagForm.get("semanticTag").value);
+    for(let tagGroup of this.arr){
+      this.tagArray = null;
+      this.postTypeService.tagSearch(tagGroup).subscribe(data => {this.tagArray = data;
+      this.list = true;
+      this.suggestionTags = new Array<WikiData>();
+      this.tags = new Array<WikiData>();
+      for(let i=0; i<Object.entries(this.tagArray)[1][1].length; i++){        
+        this.suggestionTags[i] = Object.assign({});
+        this.suggestionTags[i].label = Object.values(Object.entries(Object.entries(this.tagArray)[1])[1][1][i])[6].toString();
+        this.suggestionTags[i].qcode = Object.values(Object.entries(Object.entries(this.tagArray)[1])[1][1][i])[1].toString();
+        //console.log(JSON.stringify(this.suggestionTags[i]));
+        this.tags.push(this.suggestionTags[i]);
+        //console.log(JSON.stringify(this.tags[i]));
+      }}, error => console.log(error));
+    }
+  }
+
   add(){
     if(this.formAreaInstanceAddForm.valid){
-      let trialPost = {id: null, postText: JSON.stringify(this.formAreaInstanceAddForm.value), postTypeId: this.postType.id};
+      let trialPost = {id: null, postText: JSON.stringify(this.formAreaInstanceAddForm.value), postTypeId: this.postType.id,
+      semanticTag: JSON.stringify(this.tagForm.get("semanticTag").value), selectedTags: JSON.stringify(this.tagForm.get("selectedTags").value)};
       this.post = Object.assign({},trialPost);
     }
     this.submitted = true;
