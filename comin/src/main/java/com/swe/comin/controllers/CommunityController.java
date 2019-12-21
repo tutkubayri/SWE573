425c3ba36b009a61ae9ad14d87844fad4f00a6da
Tutku BAYRI <tutkubayri@gmail.com>
2019-12-21 22:25:41 +0200

    Deleting cross origin

--- comin/src/main/java/com/swe/comin/controllers/CommunityController.java
+++ comin/src/main/java/com/swe/comin/controllers/CommunityController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CommunityController {
 
