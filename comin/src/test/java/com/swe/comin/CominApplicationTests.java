2d8a8613fa105cc31d62867bef858d256257976d
Tutku BAYRI <tutkubayri@gmail.com>
2019-12-21 13:05:00 +0200

    Tests and deployment trial

--- comin/src/test/java/com/swe/comin/CominApplicationTests.java
+++ comin/src/test/java/com/swe/comin/CominApplicationTests.java
@@ -1,13 +1,18 @@
 package com.swe.comin;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
 class CominApplicationTests {
 
-    //@Test
-    void contextLoads() {
+    @Test
+    public void contextLoads() {
     }
-
 }
